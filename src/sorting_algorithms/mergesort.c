7145f8a604f102845a88180a9b99ce5d79d726ee
CarlosV <carlos.vargas-figueroa@grenoble-inp.org>
2021-03-09 08:19:41 +0100

    update last version

--- src/sorting_algorithms/mergesort.c
+++ src/sorting_algorithms/mergesort.c
@@ -7,6 +7,60 @@
 
 #include "sorting.h"
 
+
+/* 
+   Merge two sorted chunks of array T!
+   The two chunks are of size size
+   First chunck starts at T[0], second chunck starts at T[size]
+*/
+void merge (uint64_t *T, const uint64_t size)
+{
+  uint64_t *X = (uint64_t *) malloc (2 * size * sizeof(uint64_t)) ;
+  
+  uint64_t i = 0 ;
+  uint64_t j = size ;
+  uint64_t k = 0 ;
+  
+  while ((i < size) && (j < 2*size))
+    {
+      if (T[i] < T [j])
+	{
+	  X [k] = T [i] ;
+	  i = i + 1 ;
+	}
+      else
+	{
+	  X [k] = T [j] ;
+	  j = j + 1 ;
+	}
+      k = k + 1 ;
+    }
+
+  if (i < size)
+    {
+      for (; i < size; i++, k++)
+	{
+	  X [k] = T [i] ;
+	}
+    }
+  else
+    {
+      for (; j < 2*size; j++, k++)
+	{
+	  X [k] = T [j] ;
+	}
+    }
+  
+  memcpy (T, X, 2*size*sizeof(uint64_t)) ;
+  free (X) ;
+  
+  return ;
+}
+
+
+
+
+
 /* 
    merge sort -- sequential, parallel -- 
 */
